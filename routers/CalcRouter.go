a7c6d4ea27e816626af3de4678446e791a961559
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-05-31 17:25:09 -0300

    added a preview message to the /history on calc

--- routers/CalcRouter.go
+++ routers/CalcRouter.go
@@ -17,10 +17,6 @@ func CalcRouter(routerObj *gin.Engine) *gin.Engine {
 		})
 	})
 
-	// routerObj.GET("/os", func(c *gin.Context) {
-	// 	c.JSON(200, runtime.GOOS)
-	// })
-
 	routerObj.GET(addressPrefix+"/calc2", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello calc2!",
@@ -33,6 +29,9 @@ func CalcRouter(routerObj *gin.Engine) *gin.Engine {
 	routerObj.POST(addressPrefix+"/bin-to-hex", controller.BinToHexController)
 	routerObj.POST(addressPrefix+"/hex-to-bin", controller.HexToBinController)
 	routerObj.POST(addressPrefix+"/statistics-calc", controller.StatisticsCalculatorController)
+	routerObj.GET(addressPrefix+"/history", func(c *gin.Context) {
+		c.String(200, "**API Calculator History** \nThis should make use of the server database")
+	})
 
 	return routerObj
 }
