cb00e313920a6ad51b09665ed78271e94d411bf8
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-06-05 12:29:12 -0300

    fixed oauth env variables not accessible on func

--- config/Oauth2Config.go
+++ config/Oauth2Config.go
@@ -5,15 +5,16 @@ import (
 	"os"
 )
 
-// Oauth Config variables
-var clientID string = os.Getenv("OAUTH_CLIENT_ID")
-var clientSecret string = os.Getenv("OAUTH_CLIENT_SECRET")
-var redirectURL string = os.Getenv("OUATH_REDIRECT_URL")
-
 // Oauth 2 Config function using github
 func Oauth2Config() {
+	// Oauth Config variables
+	clientID := os.Getenv("OAUTH_CLIENT_ID")
+	clientSecret := os.Getenv("OAUTH_CLIENT_SECRET")
+	redirectURL := os.Getenv("OAUTH_REDIRECT_URL")
+
 	// for now just print the env variables
 	fmt.Println("Client ID: ", clientID)
 	fmt.Println("Client Secret: ", clientSecret)
 	fmt.Println("Redirect URL: ", redirectURL)
+
 }
