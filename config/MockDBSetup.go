132f86609dd703f9cd93040b626b37ea1cc01c7a
nicolascastellano <nicolas.castellano.6@hotmail.com>
2023-06-04 23:37:08 -0300

    added env variables on mock db

--- config/MockDBSetup.go
+++ config/MockDBSetup.go
@@ -2,17 +2,27 @@ package config
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
+	"github.com/joho/godotenv"
 	"github.com/ory/dockertest/v3"
 	_ "github.com/ory/dockertest/v3/docker"
+	"github.com/stretchr/testify/assert"
 )
 
 func MockDBSetup(t *testing.T) (*gorm.DB, error) {
+	// get the absolute path of the env file
+	// envPath, err := filepath.Join(filepath.Abs("../"), ".env")
+
 	// Initialize the Docker pool
+	// Load environment variables from .env file, this is for the local server, on production, the env variables are set on the server
+	err := godotenv.Load("../.env")
+	assert.Nil(t, err)
+
 	// Create a new instance of the Docker pool
 	pool, err := dockertest.NewPool("")
 	if err != nil {
@@ -21,10 +31,11 @@ func MockDBSetup(t *testing.T) (*gorm.DB, error) {
 
 	// Set the environment variables for the PostgreSQL container
 	env := []string{
-		"POSTGRES_USER=postgres",
-		"POSTGRES_PASSWORD=mySecretPassword",
-		"POSTGRES_DB=test_db",
-		"DOCKER_MACHINE_NAME=postgres_container",
+		// "POSTGRES_USER=postgres",
+		"POSTGRES_USER=" + os.Getenv("DB_TEST_USER"),
+		"POSTGRES_PASSWORD=" + os.Getenv("DB_TEST_PASSWORD"),
+		"POSTGRES_DB=" + os.Getenv("DB_TEST_NAME"),
+		"DOCKER_MACHINE_NAME=" + os.Getenv("DB_CONTAINER_NAME"),
 	}
 
 	// Define the Docker container run options
@@ -47,12 +58,14 @@ func MockDBSetup(t *testing.T) (*gorm.DB, error) {
 	}()
 
 	// Get the host and port for connecting to the PostgreSQL container
-	host := "192.168.0.99"
-	// port := resource.GetPort("5433/tcp")
-	port := "32783"
+	host := os.Getenv("DB_TEST_HOST")
+	port := os.Getenv("DB_TEST_PORT")
+	user := os.Getenv("DB_TEST_USER")
+	password := os.Getenv("DB_TEST_PASSWORD")
+	dbname := os.Getenv("DB_TEST_NAME")
 
 	// Create the DSN string for the PostgreSQL connection
-	dsn := fmt.Sprintf("host=%s port=%s user=postgres password=mySecretPassword dbname=test_db sslmode=disable", host, port)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	// Connect to the PostgreSQL database
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
